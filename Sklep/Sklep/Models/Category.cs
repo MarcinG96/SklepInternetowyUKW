b36c96e4e78ee5044d7af8b71b635ace9740bcb7
Marcin Górny <87368@office365.ukw.edu.pl>
2021-03-23 10:29:28 +0100

    Dodanie klas i FilmsContext

---
+++ Sklep/Sklep/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sklep.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Wprowadz nazwę kategorii")]
+        [StringLength(50)]
+
+        public string Name { get; set; }
+
+        public string Desc { get; set; }
+
+        public ICollection<Film> Films { get; set; }
+    }
+}
